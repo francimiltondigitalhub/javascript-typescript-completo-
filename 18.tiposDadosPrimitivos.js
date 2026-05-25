c04e8c2c10adf9dc470545c99f28b540c90dafd4
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-05-25 16:33:01 -0300

    aprendendo tipos de dados primitivos

---
+++ 18.tiposDadosPrimitivos.js
@@ -0,0 +1,18 @@
+//TIPOS DE DADOS PRIMITIVOS:
+
+const nome = "Luiz"; //string
+const nome2 = 'Francis'; //string
+const nome3 = `João`; //string
+
+const num = 10; //number
+const num2 = 10.52; //number
+
+let nomeAluno; //undefined --> não aponta para local nenhum na memória. Aqui é o próprio sistema que colocar se algo não for definido.
+const sobrenomeAluno = null; // não aponta para local nenhum na memória. Colocar por exemplo, quando o usuário não selecionar nada.
+
+const booleanTrue = true; //boolean
+const booleaFalse = false; //boolean
+
+
+
+
