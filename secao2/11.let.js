5f344114abb687e97705eee2ca63a9c5e8b1b74d
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-05-25 06:34:12 -0300

    criando uma variável com let

---
+++ secao2/11.let.js
@@ -0,0 +1,22 @@
+//Variável já declarada:
+let  nome = 'João'; //let = posso alterar ou redeclarar
+
+/**
+Observaçõe: 
+Não posso usar palavras reservadas.
+Preicisam ter nomes sigificativos.
+Não posso começar o nome de uma variável com um número.
+Não pode ter espaços ou traços(utilizar camelCase).
+Case-sensitive --> letra maiúcula ou minúscula faz diferença: let nomeCliente/nomecliente
+*/
+
+console.log(`${nome} nasceu em 1984`);
+console.log('Em 2000', nome, 'conheceu Maria');
+
+// Posso não declarar um valor na variável(udefined):
+let carro;
+
+//Agora eu defino:
+carro = 'Vectra';
+console.log(carro);
+
