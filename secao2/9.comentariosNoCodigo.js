2cf30a859e128f66187d829525782deaa7a9cc9f
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-05-25 05:36:35 -0300

    comentários no código

---
+++ secao2/9.comentariosNoCodigo.js
@@ -0,0 +1,18 @@
+//Escrevendo um comentário de uma linha usando //
+
+console.log('Olá mundo!'); //Um comentário
+console.log('Linha 1'); //Outro comentário
+
+//Escrevendo um comentário usando código de blocos(varias linhas)/*...*/
+
+/**
+Linha 1
+Linha 2
+Linha 3
+*/
+
+//Comentando uma linha de código para não ter que apagá-la
+
+//console.log('Linha 4') <-- inativa
+
+//Ou: usar comentário de bloco para inativar várias linhas de códigos.
