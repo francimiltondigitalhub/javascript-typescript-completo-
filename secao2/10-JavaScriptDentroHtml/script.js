477f29554db897c46182785b8d0c5a2c7ecefb61
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-05-25 06:04:50 -0300

    secao 2 - primeira pagina html com javascript

---
+++ secao2/10-JavaScriptDentroHtml/script.js
@@ -0,0 +1,7 @@
+//Não é uma boa prática escrever o código js direto no HTML, pra não ter atraso no carregamento da página e desorganização.
+
+console.log("Olá mundo!");
+console.log("Este trecho será exibido no navegador");
+
+//Será exibido no console do navegador
+
