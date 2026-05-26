bf00f52c4b0acbadcf094fcdbda9f4560d3c8cfa
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-05-25 22:31:28 -0300

    aprendendo funções

---
+++ secao2/31.funcoes.js
@@ -0,0 +1,35 @@
+const raiz = function (n) { //ou no lugar de 'function' usar =>
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+
+//jeito reduzido:
+
+const raiz0 = n => n ** 0.5;
+
+console.log(raiz0(16));
+
+
+
+//function soma(x, y) {
+//    const resultado = x + y;
+//    return resultado;
+//}
+
+//console.log(soma(12, 15));
+
+
+
+
+
+//function saudacao(nome) {
+//   return `Bom dia ${nome}!`;
+//}
+
+//const variavel = saudacao('Luiz');
+//console.log(variavel);
+
+
+//saudacao('Luiz');
+//saudacao('Pedro');
